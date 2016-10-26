f794262eec3f0fbdadb12086dd6d74f73b47e933
hstrey <Helmut.Strey@gmail.com>
2016-10-26 16:26:25 -0400

    managed to put bokeh plot on a page

--- ecg_graph/views.py
+++ ecg_graph/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+import requests
 
 # Create your views here.
+
+def index(request):
+	# api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/appl.json'
+	# session = requests.Session()
+	# session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+	# raw_data = session.get(api_url)
+
+	plot = figure()
+	plot.circle([1.2],[3,4])
+	script, div = components(plot)
+
+	template = loader.get_template('ecg_graph/index.html')
+	context = {'script': script,
+			 'div': div}
+
+	return HttpResponse(template.render(context, request))
--- ecg_website/settings.py
+++ ecg_website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ecg_graph.apps.EcgGraphConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
